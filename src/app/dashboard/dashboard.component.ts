89c6968354b35c58258a89abe2d0ff4b45d4090a
sreematta <matta.s@husky.neu.edu>
2018-08-13 19:37:55 -0400

    Added dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css', '../../assets/css/style.css']
 })
 export class DashboardComponent implements OnInit {
 
