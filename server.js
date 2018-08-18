fcb3efb94900b9ba56800d58bda7398de8b71ade
sreematta <matta.s@husky.neu.edu>
2018-08-18 16:02:14 -0400

    fix routing errors

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/music-hub-app'));
+app.use(express.static('./dist'));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join('./dist/music-hub-app/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
 });
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4200);
