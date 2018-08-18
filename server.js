bfa94bd8de229624375219d74f986dd27c66196b
sreematta <matta.s@husky.neu.edu>
2018-08-18 16:11:01 -0400

    fix heroku link

--- server.js
+++ server.js
@@ -5,12 +5,19 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist'));
+// app.use(express.static('./dist/music-hub-app'));
+//
+// app.get('/*', function(req,res) {
+//
+//   res.sendFile(path.join('./dist/music-hub-app/index.html'));
+// });
 
-app.get('/*', function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
+app.use(express.static(__dirname + '/dist/music-hub-app'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/music-hub-app/index.html'));
 });
 
-
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
