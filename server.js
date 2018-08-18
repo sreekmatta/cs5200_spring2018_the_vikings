746cd049dc6c1a3ccf9a431591013a1400327847
sreematta <matta.s@husky.neu.edu>
2018-08-18 16:16:44 -0400

    fixed heroku link with server.js

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-// app.use(express.static('./dist/music-hub-app'));
-//
-// app.get('/*', function(req,res) {
-//
-//   res.sendFile(path.join('./dist/music-hub-app/index.html'));
-// });
-
-app.use(express.static(__dirname + '/dist/music-hub-app'));
+app.use(express.static('./dist/music-hub-app'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/music-hub-app/index.html'));
+  res.sendFile(path.join('./dist/music-hub-app/index.html'));
 });
+
+// app.use(express.static(__dirname + '/dist/music-hub-app'));
+//
+// app.get('/*', function(req,res) {
+//
+//   res.sendFile(path.join(__dirname+'/dist/music-hub-app/index.html'));
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
