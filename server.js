8f72c0286ab808d88b82bfdc0cec9d67b36e03e3
sreematta <matta.s@husky.neu.edu>
2018-08-20 10:42:27 -0400

    Heroku working

--- server.js
+++ server.js
@@ -1,28 +1,11 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
 // Serve only the static files form the dist directory
-// app.use(express.static('./dist/music-hub-app'));
-//
-// app.get('/*', function(req,res) {
-//
-//   res.sendFile(path.join('./dist/music-hub-app/index.html'));
-// });
-
-// app.use(express.static(__dirname + '/dist/music-hub-app'));
-//
-// app.get('/*', function(req,res) {
-//
-//   res.sendFile(path.join(__dirname+'/dist/music-hub-app/index.html'));
-// });
-
 app.use(express.static('./dist/music-hub-app'));
-app.get('*', function (req, res) {
-  res.sendFile('index.html', { root: __dirname });
-});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.get('/*', function (req, res) {
+  // res.sendFile(path.join('./dist/course-manager-client-angular/index.html'));
+  res.sendFile(__dirname + '/dist/music-hub-app/index.html');
+});
+app.listen(process.env.PORT || 8080);
